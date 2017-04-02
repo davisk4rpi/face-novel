36d051f15cb8758c3202d0105b70c288ac86b542
davisk4rpi <davisk4rpi@gmail.com>
2017-04-02 15:11:04 -0700

    add username to signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
+ 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+ 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email.downcase!
+    end
+
 end
