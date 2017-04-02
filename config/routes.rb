90111332d02ec469c7e0754c72c5ce154e5aa5bb
davisk4rpi <davisk4rpi@gmail.com>
2017-04-02 14:23:25 -0700

    create user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  devise_for :users
   root 'static_pages#home'
 
-  get 'static_pages/about'
+  get    '/about',   to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
