9998c242c5bf8499d0ae10f38e86f979e54a85ae
davisk4rpi <davisk4rpi@gmail.com>
2017-04-14 15:12:21 -0700

    Add tests for valid signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,14 +15,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/registrations/new'
   end
 
-  '''test "invalid signup information" do
+  test "valid signup information" do
     get new_user_registration_path
-    assert_no_difference "User.count" do
-      post user_registration_path, params: { user: { name:  "",
-                                             email: "user@invalid",
-                                             password:              "foo",
-                                             password_confirmation: "bar" } }
+    assert_difference "User.count", 1 do
+      post user_registration_path, params: { user: { name:  "Tester",
+                                             email: "user@valid.com",
+                                             password:              "foobar",
+                                             password_confirmation: "foobar" } }
     end
-    assert_template "users/registrations/new"
-  end'''
+    assert_redirected_to  root_path
+  end
 end
