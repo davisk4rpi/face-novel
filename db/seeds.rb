9f34cb91a163bfea18e9cdb0c5fa60f2bbffa9f4
davisk4rpi <davisk4rpi@gmail.com>
2017-05-11 15:39:00 -0700

    Add user index render

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+             email: "example@example.org",
+             password: "foobar")
+User.create!(name:  "Example User1",
+             email: "example1@example.org",
+             password: "foobar")
+User.create!(name:  "Example User2",
+             email: "example2@example.org",
+             password: "foobar")
+User.create!(name:  "Example User3",
+             email: "example3@example.org",
+             password: "foobar")
+User.create!(name:  "Example User4",
+             email: "example4@example.org",
+             password: "foobar")
